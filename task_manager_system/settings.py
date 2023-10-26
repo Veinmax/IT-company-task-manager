1bad359306b5ddd5fc9ff02631dc34f666463725
Veinmax <aturjust88@gmail.com>
2023-10-26 21:10:12 +0300

    changing debug status for deployment

--- task_manager_system/settings.py
+++ task_manager_system/settings.py
@@ -27,7 +27,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
