e9ad1e878084db03eb749c870a17ab3f521389e5
Veinmax <aturjust88@gmail.com>
2023-10-27 00:43:47 +0300

    change debug settings with render environment

--- task_manager_system/settings.py
+++ task_manager_system/settings.py
@@ -27,7 +27,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1", "it-company-task-manager-c9zk.onrender.com"]
 
